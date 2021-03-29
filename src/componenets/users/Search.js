e25197c81d81824fc232eba4c20ed9db320ec379
peeyush24 <peeyush.pandey@truminds.co.in>
2021-03-29 23:25:51 +0530

    Added alert for empty input

--- src/componenets/users/Search.js
+++ src/componenets/users/Search.js
@@ -8,13 +8,20 @@ export class search extends Component {
 
     static propTypes = {
         searchUsers: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
+        setAlert: PropTypes.func.isRequired
     }
 
     onSubmit = e => {
         e.preventDefault();
-        this.props.searchUsers(this.state.text)
-        this.setState({text: ''})
+        
+        if(this.state.text === '') {
+            this.props.setAlert(' Please enter something', 'light');
+        } else {
+            this.props.searchUsers(this.state.text)
+            this.setState({text: ''})
+        }
     };
 
     onChange = (e) => {
@@ -22,6 +29,9 @@ export class search extends Component {
     }
 
     render() {
+
+        const  { showClear, clearUsers } = this.props
+
         return (
             <div>
                 <form onSubmit = {this.onSubmit} className="form">
@@ -34,7 +44,9 @@ export class search extends Component {
                     />
                     <input type="submit" value="Search" className="btn btn-dark btn-block"/>
                 </form>
-                <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>
+                { showClear && 
+                    ( <button className="btn btn-light btn-block" onClick = { clearUsers }>Clear</button> )
+                }
             </div>
         )
     }
